e276339f44eaad1ad855f7df392cbeb84fdbfbaa
ajithvsb <31505680+ajithvsb@users.noreply.github.com>
2017-09-23 10:55:03 -0700

    Create num.c

---
+++ num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i; clrscr();
+if(i>0)
+{
+printf("Positive");
+}
+elseif(i<0)
+{
+printf("negative");
+}
+else
+{
+printf("zero");
+}getch();
+}
